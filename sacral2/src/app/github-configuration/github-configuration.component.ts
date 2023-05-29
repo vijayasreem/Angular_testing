2ee087235f39eeffc38a905f1f9c1107833490bc
dotnet-code <@dotnet-code>
2023-05-29 14:12:33 +0530

    commit from code202305291233

---
+++ sacral2/src/app/github-configuration/github-configuration.component.ts
@@ -0,0 +1,21 @@
+﻿import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-github-configuration',
+  template: `
+    <div>
+      <p>Configuration of github</p>
+      <p>{{ error }}</p>
+    </div>
+  `,
+  styleUrls: ['./github-configuration.component.css']
+})
+export class GithubConfigurationComponent implements OnInit {
+  error = { error: "'NoneType' object has no attribute 'replace'" };
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
