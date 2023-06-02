c4bfd963f4e049f67b6ae128619f89d9f94fa699
dotnet-code <@dotnet-code>
2023-06-02 16:16:10 +0530

    commit from code202306021610

---
+++ angular/src/app/configure-github/configure-github.component.ts
@@ -0,0 +1,29 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-configure-github',
+  templateUrl: './configure-github.component.html',
+  styleUrls: ['./configure-github.component.scss']
+})
+export class ConfigureGithubComponent implements OnInit {
+
+  configureForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.configureForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+      url: ['', Validators.required],
+      repoName: ['', Validators.required],
+      noOfEntries: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    // handle the form submission here
+  }
+
+}
